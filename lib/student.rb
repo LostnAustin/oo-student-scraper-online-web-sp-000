0df9bec6ed858d745f4efef8c813c69fefcf648e
Chad Lengefeld <chadlengefeld@gmail.com>
2020-01-20 21:47:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ class Student
     attributes_hash.each do |key, value|
       #attributes = self.send(atts)
   #    binding.pry
-    self.send("#key=", value)
+    self.send("#key", value)
 
     end
   end
